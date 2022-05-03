d2044dc44dad63fcd768af86af20f915f1ef2003
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2022-05-03 09:00:00 +0200

    Přidejte soubory projektu.

---
+++ OOP_Nakladak/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Nakladak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Nakladak Tatra;
+        Nakladak Zetor;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Tatra = new Nakladak(1000, 200 , 30);
+            Tatra.Znacka = "Tatra";
+            Zetor = new Nakladak(1000, 220, 32);
+            Zetor.Znacka = "Zetor";
+
+            kamion1.Text = Tatra.Znacka + "\n";
+            kamion1.Text = "Nosnost" +  Tatra.Nosnost + "\n";
+            kamion1.Text = "Naloženo" + Tatra.Nalozeno + "\n";
+            kamion1.Text = "Objem Nádrž" + Tatra.ObjemNadrz + "\n";
+            kamion1.Text = "Spotřeba" + Tatra.Spotreba + "\n";
+            kamion1.Text = "Nosnost" + Tatra.Nosnost + "\n";
+
+
+
+
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Tatra.Naložit
+        }
+    }
+}
---
+++ OOP_Nakladak/Nakladak.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Nakladak
+{
+    public class Nakladak
+    {
+        public Nakladak(int nosnost, int objem,  int spotreba )
+        {
+            Znacka = "";
+            Nosnost = nosnost;
+            Nalozeno = 0;
+            ObjemNadrz = objem;
+            Palivo = 0;
+            Spotreba = spotreba;
+            Vzdalenost = 0;
+        }
+
+        public string Znacka { get; set; }
+        public int Nosnost { get; set; }
+        public int Nalozeno { get; set; }
+        public int ObjemNadrz { get; set; }
+        public int Palivo { get; set; }
+        public int Spotreba { get; set; }
+        public int Vzdalenost { get; set; }
+
+        public void Jet(int Vzdalenost) 
+        {
+        if (Vzdalenost)
+            {
+
+            }
+        }
+        public void Naloz()
+        {
+
+        }
+        public void Tankovat()
+        {
+            Palivo += input;
+            if (Palivo > ObjemNadrz)
+            {
+                Palivo = ObjemNadrz;
+            }
+        }
+        public
+    }
+    
+}
